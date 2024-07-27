2d3e4b9dcb73856a9d7b266bf0bd888663b489e0
Vishal Rathod <vrathod591@gmail.com>
2024-07-27 22:03:51 +0530

    editing Header Component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,8 +3,22 @@ import {LOGO_URL} from "../Utils/constant";
 
 const Header = () => {
   return (
-    <div className='w-11/12	mx-auto sticky border-grey-300 border-solid border'>
-    <img src={LOGO_URL} alt="logo" />
+    <div className='w-full	mx-auto sticky top-0 border-grey-300 border-solid border bg-white py-3 px-12 shadow-slate-900 shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
+        <div className='flex justify-between items-center'>
+            <img src={LOGO_URL} alt="logo"/>
+        </div>
+        <div className='flex justify-between items-center'>
+            <ul>
+                <li>HOME</li>
+                <li>CATEGORIES</li>
+                <li>PAGES</li>
+                <li>BRAND</li>
+                <li>BLOG</li>
+            </ul>
+        </div>
+        <div className='flex justify-between items-center'>
+                
+        </div>
     </div>
   )
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,17 @@ const Home = () => {
   return (
     <div>
         <Header/>
-        <div class>
+        <div className="h-96 bg-slate-400 border border-dotted">
+            Hello
+        </div>
+        <div className="h-96 bg-slate-400 border border-dotted">
+            Hello
+        </div>
+        <div className="h-96 bg-slate-400 border border-dotted">
+            Hello
+        </div>
+        <div className="h-96 bg-slate-400 border border-dotted">
+            Hello
         </div>
     </div>
   )

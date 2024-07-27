95cd922e6fc31a47558e419ea10017770a1d4338
Vishal Rathod <vrathod591@gmail.com>
2024-07-27 18:41:39 +0530

    App setup and TailwindCss setup Done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Body from './Components/Body';
+
+function App() {
+  return (
+    <div className="">
+     <Body/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Utils/constant.js
@@ -0,0 +1,1 @@
+export const LOGO_URL = "https://teeperfect.myshopify.com/cdn/shop/t/10/assets/logo-black-br-h5.png"

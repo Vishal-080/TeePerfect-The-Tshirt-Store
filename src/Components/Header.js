534b1508ba16d1a1c0c10f44f8ab0a144701a595
Vishal Rathod <vrathod591@gmail.com>
2024-07-31 02:56:58 +0530

    Header CSS completeed

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,31 +9,31 @@ import { BsPersonCircle } from "react-icons/bs";
 
 const Header = () => {
   return (
-    <div className="w-full	mx-auto sticky top-0 border-grey-300 border-solid border bg-white py-3 px-12 shadow-slate-900 shadow-[0_3px_10px_rgb(0,0,0,0.2)] flex justify-between">
+    <div className="w-full	mx-auto sticky top-0 border-grey-300 border-solid border bg-white py-2.5 px-12 shadow-slate-900 shadow-[0_3px_10px_rgb(0,0,0,0.2)] flex justify-between">
       <div className="flex justify-between items-center ">
         <img src={LOGO_URL} alt="logo" />
       </div>
-      <div className="flex justify-between items-center">
-        <ul className="flex justify-between items-center mx-2 px-4 border border-red-500">
-          <li className="mx-2 font-medium text-center text-md">HOME</li>
+      <div className="flex justify-between items-center w-4/12">
+        <ul className="flex justify-between items-center w-full">
+          <li  className="mx-2 font-medium text-center text-md">HOME</li>
           <li className="mx-2 font-medium text-center text-md">CATEGORIES</li>
           <li className="mx-2 font-medium text-center text-md">PAGES</li>
           <li className="mx-2 font-medium text-center text-md">BRAND</li>
           <li className="mx-2 font-medium text-center text-md">BLOG</li>
         </ul>
       </div>
-      <div>
-        <ul className="flex justify-between items-center">
-          <li>
+      <div className="flex justify-between items-center w-1/4">
+        <ul className="flex justify-between items-center mx-2 px-4 w-full">
+          <li className="mx-2">
             <AiOutlineSearch />
           </li>
-          <li>
+          <li className="mx-2">
             <AiOutlineHeart />
           </li>
-          <li>
+          <li className="mx-2">
             <BsPersonCircle />
           </li>
-          <li>
+          <li className="mx-2">
             <AiOutlineShoppingCart />
           </li>
         </ul>

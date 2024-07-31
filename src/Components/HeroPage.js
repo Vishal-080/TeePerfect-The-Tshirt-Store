28258c19f7a8fba62d895b36c4187976fd39dcc5
Vishal Rathod <vrathod591@gmail.com>
2024-07-31 19:26:18 +0530

    Hero page created

---
+++ src/Components/HeroPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeroPage = () => {
+  return (
+    <div className='w-full m-auto bg-red-400 '>
+        <img className='max-w-full object-cover h-auto' src="https://teeperfect.myshopify.com/cdn/shop/files/p4.jpg" alt="HeroImg" />
+    </div>
+  )
+}
+
+export default HeroPage;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import Header from './Header';
+import HeroPage from './HeroPage';
 
 const Home = () => {
   return (
     <div>
         <Header/>
-        <div className="h-96 bg-slate-400 border border-dotted">
-            Hello
-        </div>
+        <HeroPage/>
         <div className="h-96 bg-slate-400 border border-dotted">
             Hello
         </div>

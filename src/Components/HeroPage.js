5918d6aa286417f694dbabe309107a208a0c6540
Vishal Rathod <vrathod591@gmail.com>
2024-08-01 13:36:13 +0530

    HeroPage css added

--- src/Components/HeroPage.js
+++ src/Components/HeroPage.js
@@ -6,7 +6,7 @@ const HeroPage = () => {
       <div className='z-10 left-64 top-2/4 absolute flex flex-col justify-center items-center'>
         <h1 className='m-2 font-bold text-5xl text-[#3ba9e0] font-serif text-center'>SHIRTS AS UNIQUE AS YOU ARE</h1>
         <h4 className='m-2 text-center font-normal text-xl'>Welcome To The Largest Online T-shirt Platform In The World</h4>
-        <button className='m-2 text-center border py-2 px-4 rounded-md bg-white text-[#3ba9e0] font-bold w-32 m-auto'>Shop Now</button>
+        <button className='m-2 text-center border py-2 px-4 rounded-md bg-white text-[#3ba9e0] font-semibold text-lg'>Shop Now</button>
       </div>
         <img className='max-w-full object-cover h-screen' src="https://teeperfect.myshopify.com/cdn/shop/files/p4.jpg" alt="HeroImg" />
     </div>

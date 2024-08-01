9d680195591e6819e6f3c21367c5491b9ff68654
Vishal Rathod <vrathod591@gmail.com>
2024-08-01 13:35:47 +0530

    HeroPage added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,7 +9,7 @@ import { BsPersonCircle } from "react-icons/bs";
 
 const Header = () => {
   return (
-    <div className="w-full	mx-auto sticky top-0 border-grey-300 border-solid border bg-white py-2.5 px-12 shadow-slate-900 shadow-[0_3px_10px_rgb(0,0,0,0.2)] flex justify-between">
+    <div className="w-full	mx-auto sticky top-0 border-grey-300 border-solid border bg-white py-2.5 px-12 shadow-slate-900 shadow-[0_3px_10px_rgb(0,0,0,0.2)] flex justify-between z-50">
       <div className="flex justify-between items-center ">
         <img src={LOGO_URL} alt="logo" />
       </div>
--- src/Components/HeroPage.js
+++ src/Components/HeroPage.js
@@ -3,7 +3,12 @@ import React from 'react'
 const HeroPage = () => {
   return (
     <div className='w-full m-auto bg-red-400 '>
-        <img className='max-w-full object-cover h-auto' src="https://teeperfect.myshopify.com/cdn/shop/files/p4.jpg" alt="HeroImg" />
+      <div className='z-10 left-64 top-2/4 absolute flex flex-col justify-center items-center'>
+        <h1 className='m-2 font-bold text-5xl text-[#3ba9e0] font-serif text-center'>SHIRTS AS UNIQUE AS YOU ARE</h1>
+        <h4 className='m-2 text-center font-normal text-xl'>Welcome To The Largest Online T-shirt Platform In The World</h4>
+        <button className='m-2 text-center border py-2 px-4 rounded-md bg-white text-[#3ba9e0] font-bold w-32 m-auto'>Shop Now</button>
+      </div>
+        <img className='max-w-full object-cover h-screen' src="https://teeperfect.myshopify.com/cdn/shop/files/p4.jpg" alt="HeroImg" />
     </div>
   )
 }

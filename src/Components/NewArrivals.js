543021c29c8579000caa85507c0b509925cf5de4
Vishal Rathod <vrathod591@gmail.com>
2024-08-04 23:36:11 +0530

    New Arrival css editing...

--- src/Components/NewArrivals.js
+++ src/Components/NewArrivals.js
@@ -1,18 +1,28 @@
 import React from "react";
 
 const NewArrivals = () => {
-  return <div className="">
-    <div>
+  return (
+    <div className="">
+      <div className="flex justify-between items-center">
         <h1 className="">New Arrivals</h1>
         <h2 className="text-[#3ba9e0]">View More</h2>
+      </div>
+      <div>
+        <div>
+          <img src="" alt="productImg" />
+        </div>
+        <div>
+          <img src="" alt="productImg" />
+        </div>
+        <div>
+          <img src="" alt="productImg" />
+        </div>
+        <div>
+          <img src="" alt="productImg" />
+        </div>
+      </div>
     </div>
-    <div>
-        <div><img src="" alt="productImg"/></div>
-        <div><img src="" alt="productImg"/></div>
-        <div><img src="" alt="productImg"/></div>
-        <div><img src="" alt="productImg"/></div>
-    </div>
-  </div>;
+  );
 };
 
 export default NewArrivals;

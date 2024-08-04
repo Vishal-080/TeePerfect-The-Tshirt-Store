88de950fe35997a222cb473f0496f989caf03c7c
Vishal Rathod <vrathod591@gmail.com>
2024-08-04 23:56:43 +0530

    New Arrival editing

--- src/Components/NewArrivals.js
+++ src/Components/NewArrivals.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const NewArrivals = () => {
   return (
-    <div className="">
+    <div className="w-10/12 m-auto">
       <div className="flex justify-between items-center">
         <h1 className="">New Arrivals</h1>
-        <h2 className="text-[#3ba9e0]">View More</h2>
+        <h2 className="font-semibold text-md text-[#3ba9e0]">View More</h2>
       </div>
       <div>
         <div>

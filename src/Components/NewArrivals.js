941ef4adefca33213a9e4d62336a4a7507b6da9a
Vishal Rathod <vrathod591@gmail.com>
2024-08-06 03:05:52 +0530

    New arrival css editing.

--- src/Components/NewArrivals.js
+++ src/Components/NewArrivals.js
@@ -15,7 +15,7 @@ const NewArrivals = () => {
 
   return (
     <div className="w-10/12 mx-auto my-10 p-2">
-      <div className="flex justify-between items-center bg-blue-300">
+      <div className="flex justify-between items-center">
         <h1 className="font-bold text-lg">New Arrivals</h1>
         <h2 className="font-bold text-md cursor-pointer text-[#3ba9e0] hover:border-b-2 hover:border-[#3ba9e0]">
           View More

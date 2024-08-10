cca2e20bc332f4a401045518fdf4f5df4ee4e907
Vishal Rathod <vrathod591@gmail.com>
2024-08-11 01:11:28 +0530

    Header logo-size reduced

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,25 +10,25 @@ import {Link} from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div className="w-full	mx-auto sticky top-0 border-grey-300 border-solid border bg-white py-2.5 px-12 shadow-slate-900 shadow-[0_3px_10px_rgb(0,0,0,0.2)] flex justify-between z-50">
+    <div className="w-full	mx-auto sticky top-0 border-grey-300 border-solid border bg-white py-1 px-10 shadow-slate-900 shadow-[0_3px_10px_rgb(0,0,0,0.2)] flex justify-between z-50">
       <div className="flex justify-between items-center ">
-        <img src={LOGO_URL} alt="logo" />
+        <img className="w-32" src={LOGO_URL} alt="logo" />
       </div>
       <div className="flex justify-between items-center w-4/12">
         <ul className="flex justify-between items-center w-full">
-          <li className="mx-2 font-medium text-center text-sm  cursor-pointer">
+          <li className="mx-2 font-medium text-center text-xs  cursor-pointer">
             <Link to={"/"}>HOME</Link>
           </li>
-          <li className="mx-2 font-medium text-center text-sm cursor-pointer">
+          <li className="mx-2 font-medium text-center text-xs cursor-pointer">
             <Link to={"/categories"}>CATEGORIES</Link>
           </li>
-          <li className="mx-2 font-medium text-center text-sm cursor-pointer">
+          <li className="mx-2 font-medium text-center text-xs cursor-pointer">
             <Link to={"/pages"}>PAGES</Link>
           </li>
-          <li className="mx-2 font-medium text-center text-sm cursor-pointer">
+          <li className="mx-2 font-medium text-center text-xs cursor-pointer">
             <Link to={"/brand"}>BRAND</Link>
           </li>
-          <li className="mx-2 font-medium text-center text-sm cursor-pointer">
+          <li className="mx-2 font-medium text-center text-xs cursor-pointer">
             <Link to={"/blog"}>BLOG</Link>
           </li>
         </ul>

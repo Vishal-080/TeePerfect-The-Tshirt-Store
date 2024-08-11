eb07276c5af274b30b762d25be4a7555c5858a8b
Vishal Rathod <vrathod591@gmail.com>
2024-08-12 01:27:36 +0530

    Banner editing

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -6,7 +6,7 @@ const Banner = () => {
         <div className="bg-BANNER_BG_IMG_URL w-11/12 m-auto h-screen bg-cover bg-no-repeat bg-top text-center text-white flex justify-center items-center flex-col">
             <h2 className='font-bold text-3xl mb-16'>Time To Travel</h2>
             <p className='font-normal text-wrap text-sm mb-16 w-1/3'>Let our gorgeous T-shirt help you to have a meaningful trip !</p>
-            <button className='bg-white text-[#3ba9e0] font-semibold text-base hover:text-white hover:bg-[#3ba9e0] py-2  px-4 rounded-md'><Link to="categories">Browse Collection</Link></button>
+            <button className='bg-white text-[#3ba9e0] font-semibold text-base hover:text-white hover:bg-[#3ba9e0] py-1  px-4 rounded-sm'><Link to="categories">Browse Collection</Link></button>
         </div>
   )
 }

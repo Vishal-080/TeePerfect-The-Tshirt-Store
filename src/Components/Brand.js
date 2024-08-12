d01bfde30e28b576af1753f990c740fd2225f263
Vishal Rathod <vrathod591@gmail.com>
2024-08-13 00:02:05 +0530

    Brand code formatted

--- src/Components/Brand.js
+++ src/Components/Brand.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Brand = () => {
-  return (
-    <div>Brand page</div>
-  )
-}
+  return <div>Brand page</div>;
+};
 
 export default Brand;

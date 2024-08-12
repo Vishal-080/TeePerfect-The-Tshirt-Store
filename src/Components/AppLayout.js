3116ed7a6e337f457ff33726c7e10372d850a6a5
Vishal Rathod <vrathod591@gmail.com>
2024-08-13 00:03:50 +0530

    AppLayout code formatted

--- src/Components/AppLayout.js
+++ src/Components/AppLayout.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import Header from './Header';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import Header from "./Header";
+import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
   return (
     <div>
-        <Header/>
-        <Outlet/>
+      <Header />
+      <Outlet />
     </div>
-  )
-}
+  );
+};
 
 export default AppLayout;

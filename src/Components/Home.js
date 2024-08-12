a4f76d696f8856a6055274def619a497ceff87d1
Vishal Rathod <vrathod591@gmail.com>
2024-08-12 23:52:38 +0530

    Home code formatted

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,31 +1,23 @@
-import React from 'react'
-import Header from './Header';
-import HeroPage from './HeroPage';
-import NewArrivals from './NewArrivals';
-import Banner from './Banner';
+import React from "react";
+import Header from "./Header";
+import HeroPage from "./HeroPage";
+import NewArrivals from "./NewArrivals";
+import Banner from "./Banner";
 
 const Home = () => {
   return (
     <div>
-        {/* <Header/> */}
-        <HeroPage/>
-        <NewArrivals/>
-        <Banner/>
-        <NewArrivals/>
-        <div className="h-96 bg-slate-400 border border-dotted">
-            Hello
-        </div>
-        <div className="h-96 bg-slate-400 border border-dotted">
-            Hello
-        </div>
-        <div className="h-96 bg-slate-400 border border-dotted">
-            Hello
-        </div>
-        <div className="h-96 bg-slate-400 border border-dotted">
-            Hello
-        </div>
+      {/* <Header/> */}
+      <HeroPage />
+      <NewArrivals />
+      <Banner />
+      <NewArrivals />
+      <div className="h-96 bg-slate-400 border border-dotted">Hello</div>
+      <div className="h-96 bg-slate-400 border border-dotted">Hello</div>
+      <div className="h-96 bg-slate-400 border border-dotted">Hello</div>
+      <div className="h-96 bg-slate-400 border border-dotted">Hello</div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;

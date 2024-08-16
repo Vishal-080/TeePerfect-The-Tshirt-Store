6fd4ced23a7ffdaa8b9e7e0ea3d2ade9e7951a1b
Vishal Rathod <vrathod591@gmail.com>
2024-08-17 00:44:54 +0530

    css editing

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Categories = () => {
-  return <div className="w-full p-12 text-2xl my-auto text-center">All Categories</div>;
+  return (
+    <div className="w-full p-12 text-2xl my-auto text-center">
+      All Categories
+    </div>
+  );
 };
 
 export default Categories;

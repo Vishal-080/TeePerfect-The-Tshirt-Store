5cd584bebd7a52f0f77e751c265ab0c3b84c1b00
Vishal Rathod <vrathod591@gmail.com>
2024-08-17 00:44:37 +0530

    css editing

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Blog = () => {
-  return <div className="w-full p-12 text-2xl my-auto text-center">My Blog</div>;
+  return (
+    <div className="w-full p-12 text-2xl my-auto text-center">My Blog</div>
+  );
 };
 
 export default Blog;

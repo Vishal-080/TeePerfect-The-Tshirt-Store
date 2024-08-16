af8524cacb66255731928268399d6b1d3b5c9bae
Vishal Rathod <vrathod591@gmail.com>
2024-08-17 00:45:10 +0530

    css editing

--- src/Components/Pages.js
+++ src/Components/Pages.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Pages = () => {
-  return <div className="w-full p-12 text-2xl my-auto text-center">My Pages</div>;
+  return (
+    <div className="w-full p-12 text-2xl my-auto text-center">My Pages</div>
+  );
 };
 
 export default Pages;

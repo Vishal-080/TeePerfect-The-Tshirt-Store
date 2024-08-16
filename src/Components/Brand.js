ffb246cbf551e0e840f56e5c77b15358f34fcf11
Vishal Rathod <vrathod591@gmail.com>
2024-08-17 00:45:35 +0530

    css editing

--- src/Components/Brand.js
+++ src/Components/Brand.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Brand = () => {
-  return <div className="w-full p-12 text-2xl my-auto text-center">Brand page</div>;
+  return (
+    <div className="w-full p-12 text-2xl my-auto text-center">Brand page</div>
+  );
 };
 
 export default Brand;
